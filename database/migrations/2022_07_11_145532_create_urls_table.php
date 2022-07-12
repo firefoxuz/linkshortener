4799bb23b5b33de322e146a874b617d84bf62e15
Sultonov Daler <daler.sultonov.00@gmail.com>
2022-07-12 15:40:41 +0500

    Added migration file

---
+++ database/migrations/2022_07_11_145532_create_urls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->string('long_url', 2048)->comment('2048 is max url length');
+            $table->string('short_url', 2048)->nullable()->comment('2048 is max url length');
+            $table->unsignedInteger('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
